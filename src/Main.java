7d0f5977fcf980c6b7ac0589783f7177a44d0e93
EduardCoool <proskurineduard@gmail.com>
2022-11-25 01:49:17 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) throws WrongInput {
         Scan creditInfo = new Scan(); // обявление объекта класса
-        creditInfo.getInputData(); // использование метода класса Scan
+        CreditData info = creditInfo.getInputData();
         Сalculate service = new Сalculate();
-        CreditData lol = new CreditData(creditInfo.sum,creditInfo.payment,creditInfo.percent,creditInfo.client);
 
-        System.out.println("Переплата по процентам = " + service.calc(lol.getSum(), lol.getPayment(),  lol.getPercent(),  lol.getClient()));
+
+        System.out.println("Переплата по процентам = " + service.calc(info.getSum(), info.getPayment(),  info.getPercent(),  info.getClient()));
 
 
 
--- src/Scan.java
+++ src/Scan.java
@@ -2,33 +2,27 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Scan {
-    double  sum;
-    double  payment;
-    double percent;
-    String client;
 
-    public void getInputData() throws WrongInput {
+
+    public CreditData getInputData() throws WrongInput {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Введите сумму, ежемесячный платёж, процент, тип клиента");
         String s = in.nextLine();
         String[] array = s.split(" ");// массив строк
 
-         sum = Double.parseDouble(array[0]);
-         payment = Double.parseDouble(array[1]);
-         percent = Double.parseDouble(array[2]);
-         client = array[3];
+        CreditData datainfo = new CreditData(Double.parseDouble(array[0]),Double.parseDouble(array[1]),Double.parseDouble(array[2]),array[3] );
 
-        if (sum <= 0 || payment <= 0 || percent <= 0) {
+        if (datainfo.getSum() <= 0 || datainfo.getPayment() <= 0 || datainfo.getPercent() <= 0) {
             throw new WrongInput("Неверный ввод данных! Введите корректные значения суммы кредита, ежемесячного платежа, процентную ставку.");
-        } else if (payment * 12 <= sum * percent / 100) {
+        } else if (datainfo.getPayment() * 12 <= datainfo.getSum() * datainfo.getPercent() / 100) {
             throw new WrongInput("Кредит невозможно выплатить, такой лучше не брать.");
-        } else if (!Objects.equals(client, "human") && !Objects.equals(client, "business")) {
+        } else if (!Objects.equals(datainfo.getClient(), "human") && !Objects.equals(datainfo.getClient(), "business")) {
             throw new WrongInput("Неверный ввод типа клиета! Введите human или business.");
 
 
         }
-
+        return datainfo ;
     }
 
 
