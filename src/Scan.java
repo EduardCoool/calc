fa47a9b090a680d3f6f21b10a64464658dfbbd67
EduardCoool <proskurineduard@gmail.com>
2022-11-24 17:34:40 +0300

    -

--- src/Scan.java
+++ src/Scan.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Scan {
@@ -20,6 +21,12 @@ public class Scan {
 
         if (sum <= 0 || payment <= 0 || percent <= 0) {
             throw new WrongInput("Неверный ввод данных! Введите корректные значения суммы кредита, ежемесячного платежа, процентную ставку.");
+        } else if (payment * 12 <= sum * percent / 100){
+            throw new WrongInput("Кредит невозможно выплатить, такой лучше не брать.");
+        } else if (!Objects.equals(client, "human") ||!Objects.equals(client, "business")) {
+            throw new WrongInput("Неверный ввод типа клиета! Введите human или business.");
+
+
         }
 
     }
--- src/Сalculate.java
+++ src/Сalculate.java
@@ -2,16 +2,8 @@ public class Сalculate {
     public double cost;
 
     public double calc(double sum, double payment, double percent, String client) throws WrongInput {
-        if (payment * 12 <= sum * percent / 100) {
-            throw new WrongInput("Кредит невозможно выплатить, такой лучше не брать.");
-        }
-        if (client.equals("human")) {
 
-        } else if (client.equals("business")) {
-            sum = sum - payment * 12;
-        } else {
-            throw new WrongInput("Неверный ввод типа клиета! Введите human или business.");
-        }
+
         while (sum > 0) {
 
             cost = cost + sum * percent / 100;
