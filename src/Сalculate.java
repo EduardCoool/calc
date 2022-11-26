c0a8ea3124ceae9fc67e1d8d3f4a895aae755283
EduardCoool <proskurineduard@gmail.com>
2022-11-26 14:14:26 +0300

    -

--- src/Сalculate.java
+++ src/Сalculate.java
@@ -1,14 +1,18 @@
 public class Сalculate {
     public double cost;
+    public double costsum;
 
     public double calc(double sumcredit, double paymentcredit, double percentcredit, String clientcredit) {
         if (clientcredit.equals("business")) {
             sumcredit = sumcredit - paymentcredit * 12;
         }
         while (sumcredit > 0) {
-            cost = cost + sumcredit * percentcredit / 100;
+            costsum = costsum + sumcredit * percentcredit / 100;
+            cost = sumcredit * percentcredit / 100;
+
             sumcredit = sumcredit - paymentcredit * 12 + cost;
+
         }
-        return cost;
+        return costsum;
     }
 }
